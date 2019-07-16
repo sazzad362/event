369963e426971754c7b134770a390cb51571dd2a
Md Sazzad Hossain <sazzad362@gmail.com>
2019-07-16 23:53:35 +0600

    Event details page update

--- js/app.js
+++ js/app.js
@@ -8,7 +8,23 @@
    *----------------------------------
    */
 	$(document).ready(function() {
-    
+
+		/*======================
+			For One page scroll
+		======================*/
+    	$('a[href*="#"]:not([href="#"])').click(function() {
+    	  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    	    var target = $(this.hash);
+    	    var headerH='0';
+    	    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    	    if (target.length) {
+    	      $('html, body').animate({
+    	        scrollTop: target.offset().top-headerH+"px"
+    	      }, 1000);
+    	      return false;
+    	    }
+    	  }
+    	});
 	});// DOM Ready
 
 }(jQuery)); // IIFE
